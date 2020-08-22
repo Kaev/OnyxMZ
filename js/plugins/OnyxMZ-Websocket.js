d2272dfbee8c2e4b64385db3ad8f811a9c154b91
Kaev <demonhunter@hotmail.de>
2020-08-23 00:26:03 +0200

    Added client and serverside heartbeat

--- js/plugins/OnyxMZ-Websocket.js
+++ js/plugins/OnyxMZ-Websocket.js
@@ -24,32 +24,48 @@
 
 
 OnyxMZ.WebSocket = require('ws');
-console.log(PluginManager.parameters('OnyxMZ-Websocket')['Server']);
 OnyxMZ.Server = `ws://${PluginManager.parameters('OnyxMZ-Websocket')['Server']}:${PluginManager.parameters('OnyxMZ-Websocket')['Port']}`;
 OnyxMZ.Connection = new OnyxMZ.WebSocket(OnyxMZ.Server);
 
 // Event for opened server connection
-OnyxMZ.Connection.onopen = function() {
+OnyxMZ.Connection.on('open', function() {
     let data = {
         data: 'Opened Connection'
     };
     OnyxMZ.Send(data);
-}
+    OnyxMZ.Heartbeat();
+});
 
 // Event for socket errors
-OnyxMZ.Connection.onerror = function(e) {
+OnyxMZ.Connection.on('error', function(e) {
     console.log(`WebSocket error: ${e}`);
-}
+});
 
 // Event for received server messages
-OnyxMZ.Connection.onmessage = function(e) {
+OnyxMZ.Connection.on('message', function(e) {
     //let received = JSON.parse(e.data);
     console.log(`Received: ${e.data}`);
-}
+});
+
+// Event for connection closed
+OnyxMZ.Connection.on('close', function() {
+    console.log(`Connection closed.`);
+    clearTimeout(this.pingTimeout);
+});
+
+// Heartbeat
+OnyxMZ.Heartbeat = function() {
+    clearTimeout(OnyxMZ.Connection.pingTimeout);
+    OnyxMZ.Connection.pingTimeout = setTimeout(() => {
+        console.log(`Terminating connection, heartbeat failed.`);
+        OnyxMZ.Connection.terminate();
+    }, 11000);
+};
+OnyxMZ.Connection.on('ping', OnyxMZ.Heartbeat);
 
 // Stringify an object and send it to the server
 OnyxMZ.Send = function(data) {
     var json = JSON.stringify(data);
     OnyxMZ.Connection.send(json);
     console.log(`Sent: ${json}`);
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({port: 1337});
 
+// Event for new client connection
 wss.on('connection', ws => {
-    ws.on('message', msg => {
-        console.log(`Received: ${msg}`);
-        /*let data = {
-            data: `You sent: ${msg}`
-        }*/
-        ws.send(msg);
+    ws.isAlive = true;
+    ws.on('pong', () => {
+        ws.isAlive = true;
     });
+    console.log(`${ws._socket.remoteAddress}:${ws._socket.remotePort} connected.`);
 });
 
+// Event for client message received
+wss.on('message', (ws, msg) => {
+    console.log(`Received: ${msg}`);
+    /*let data = {
+        data: `You sent: ${msg}`
+    }*/
+    ws.send(msg);
+});
+
+// Event for client disconnected
+wss.on('close', (ws, e) => {
+    console.log(`Client ${ws._socket.remoteAddress}:${ws._socket.remotePort} disconnected: Code ${e.code}`);
+})
+
+// Serverside heartbeat
+function heartbeat() {
+    wss.clients.forEach(function (ws) {
+        if (ws.isAlive === false)
+            return ws.terminate();
+        ws.isAlive = false;
+        ws.ping();
+    });
+}
+setInterval(heartbeat, 10000);
